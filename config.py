1efe8edfa3aa93b71c11812c3f484089f9eac5fc
Tony David <tonydavidx@gmail.com>
2025-07-31 11:17:27 +0530

    Fixed telegram wrong env variable names which caused telegram sender to fail

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
+
 # Path Configuration
 # Get the absolute path of the directory where the script is located
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
@@ -11,8 +12,8 @@ EMAIL_FROM = os.getenv("WHC_FROM_EMAIL")
 EMAIL_TO = os.getenv("WHC_TO_EMAIL")
 EMAIL_PASSWORD = os.getenv("WHC_EMAIL_PASS")
 
-TELEGRAM_CHAT_ID = os.getenv("CHAT_ID")
-TELEGRAM_ACCESS_TOKEN = os.getenv("AMZ_TL_ACCESS_TOKEN")
+TELEGRAM_CHAT_ID = os.getenv("PT_CHAT_ID")
+TELEGRAM_ACCESS_TOKEN = os.getenv("PT_TEL_ACCESS_TOKEN")
 
 # Selenium Configuration
 HEADLESS = True  # Set to True for headless mode
